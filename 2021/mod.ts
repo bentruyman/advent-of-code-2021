4ffbcf1d3feb07780843945ac9f50cb34a9dd2fe
Ben Truyman <ben@truyman.com>
2021-12-04 16:35:13 -0800

    feat(2021): add day 4

--- 2021/mod.ts
+++ 2021/mod.ts
@@ -1,6 +1,7 @@
 import * as day1 from "./solution/day1.ts";
 import * as day2 from "./solution/day2.ts";
 import * as day3 from "./solution/day3.ts";
+import * as day4 from "./solution/day4.ts";
 
 async function readInput(day: number): Promise<string> {
   const decoder = new TextDecoder();
@@ -32,6 +33,12 @@ async function solveDay3(): Promise<void> {
   console.log("Day 3", day3.part1(data), day3.part2(data));
 }
 
+async function solveDay4(): Promise<void> {
+  const data = await readInput(4);
+  console.log("Day 4", day4.part1(data), day4.part2(data));
+}
+
 await solveDay1();
 await solveDay2();
 await solveDay3();
+await solveDay4();
---
+++ 2021/solution/day4.ts
@@ -0,0 +1,90 @@
+export function part1(input: string) {
+  const { draws, boards } = parseInput(input);
+
+  for (const draw of draws) {
+    for (const board of boards) {
+      board.mark(draw);
+      if (board.hasWon()) {
+        return sumUnmarked(board) * draw;
+      }
+    }
+  }
+}
+
+export function part2(input: string) {
+  const { draws, boards } = parseInput(input);
+  const winners: Board[] = [];
+
+  for (const draw of draws) {
+    for (const board of boards) {
+      board.mark(draw);
+      if (board.hasWon()) {
+        if (boards.size === 1) {
+          return sumUnmarked(board) * draw;
+        } else {
+          boards.delete(board);
+        }
+      }
+    }
+  }
+}
+
+export function parseInput(input: string): {
+  draws: number[];
+  boards: Set<Board>;
+} {
+  const chunks = input.split("\n\n");
+  const draws = chunks[0].split(",").map((n) => parseInt(n));
+  const boards: Board[] = chunks.slice(1).map((chunk) => new Board(chunk));
+
+  return {
+    draws,
+    boards: new Set(boards),
+  };
+}
+
+class Board {
+  marks: boolean[][];
+  grid: number[][];
+
+  constructor(board: string) {
+    this.grid = board
+      .split("\n")
+      .map((row) => row.trim())
+      .map((row) => row.split(/\s+/).map((n) => parseInt(n)));
+
+    this.marks = this.grid.map((row) => new Array(row.length).fill(false));
+  }
+
+  hasWon(): boolean {
+    return (
+      this.marks.some((row) => !row.includes(false)) ||
+      transpose(this.marks).some((row) => !row.includes(false))
+    );
+  }
+
+  mark(num: number): void {
+    this.grid.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        if (col === num) {
+          this.marks[rowIndex][colIndex] = true;
+        }
+      });
+    });
+  }
+}
+
+function sumUnmarked(board: Board): number {
+  let sum = 0;
+
+  board.marks.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      if (col === false) sum += board.grid[rowIndex][colIndex];
+    });
+  });
+
+  return sum;
+}
+
+const transpose = <T>(list: T[][]): T[][] =>
+  list[0].map((_, colIndex) => list.map((row) => row[colIndex]));
